2e36001aaee64a7c4710c1f4717ecfd564076529
liwenke1 <949484025@qq.com>
2023-06-10 12:40:45 +0800

    feat: add result trace page
    add trace result table

--- src/pages/Layout/Result/ResultTrace/index.js
+++ src/pages/Layout/Result/ResultTrace/index.js
@@ -4,13 +4,81 @@
  * 以表的形式展示归属的结果，包括ULR、危险程度、相似度和相似特征等信息
  */
 
-import React from "react";
+import React, { useState } from "react";
 import ResultHeader from "@/pages/Layout/Result/ResultHeader";
+import { useStore } from "@/store";
+import { Table, Modal, Button } from "antd";
+import { useSearchParams } from "react-router-dom/dist";
 
-const ResultTrace = () => (
-    <>
-        <ResultHeader></ResultHeader>
-    </>
-);
+const { Column } = Table
+
+const ResultTrace = () => {
+    const { resultStore } = useStore()
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+    const [params] = useSearchParams()
+    let task_id = parseInt(params.get("task"))
+    if (isNaN(task_id)) task_id = "请选择扫描任务";
+    return (
+        <>
+            <ResultHeader></ResultHeader>
+            <Table dataSource={resultStore.getSimilarRepository(task_id)}>
+                <Column
+                    dataIndex="url"
+                    title="Url"
+                    key="url"
+                    render={
+                        (url, record) => {
+                            return (<a href={url}>{url.substring(8,)}</a>)
+                        }
+                    }
+                    sorter={(rowA, rowB) => rowA.url < rowB.url}
+                />
+                <Column
+                    dataIndex="type"
+                    title="类型"
+                    key="type"
+                    sorter={(rowA, rowB) => rowA.type < rowB.type}
+                />
+                <Column
+                    dataIndex="similarity_score"
+                    title="相似度分数"
+                    key="similarity_score"
+                    sorter={(rowA, rowB) => rowA.similarity_score < rowB.similarity_score}
+                />
+                <Column
+                    dataIndex="malicious_level"
+                    title="恶意等级"
+                    key="malicious_level"
+                    sorter={(rowA, rowB) => rowA.malicious_level < rowB.malicious_level}
+                />
+                <Column
+                    dataIndex="features"
+                    title="关联特征"
+                    key="features"
+                    render={(features, record) => {
+                        return (
+                            <Button type="primary" onClick={showModal}>
+                                Open Modal
+                            </Button>
+                        )
+                    }
+                    }
+                />
+            </Table>
+            <Modal title="关联特征" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <Table></Table>
+            </Modal>
+        </>
+    );
+}
 
 export default ResultTrace
--- src/store/result.Store.js
+++ src/store/result.Store.js
@@ -8,6 +8,105 @@ import { http } from "@/utils";
 
 class ResultStore {
 
+    /**
+     * @description 代码归属测试数据
+     */
+    trace_result_list = [
+        {
+            id: 1,
+            name: "测试 1",
+            url: "https://github.com/THUDM/VisualGLM-6B",
+            type: "代码溯源分析",
+            language: 'Python',
+            is_effective: true,
+            features: {
+                layout_feature: 2
+            },
+            similar_repository: [
+                {
+                    name: "ml-research/XIConceptLearning",
+                    url: "https://github.com/ml-research/XIConceptLearning",
+                    type: "repository",
+                    similarity_score: 90,
+                    malicious_level: 1,
+                    features: {
+                        layout_feature: 7
+                    }
+                },
+                {
+                    name: "MBCProject/mbc-markdown",
+                    url: "https://github.com/MBCProject/mbc-markdown",
+                    type: "repository",
+                    similarity_score: 90,
+                    malicious_level: 2,
+                    features: {
+                        layout_feature: 4
+                    }
+                },
+                {
+                    name: "Vision-CAIR/MiniGPT-4",
+                    url: "https://github.com/Vision-CAIR/MiniGPT-4",
+                    type: "repository",
+                    similarity_score: 80,
+                    malicious_level: 5,
+                    features: {
+                        layout_feature: 7
+                    }
+                },
+                {
+                    name: "yueyueL/DL-based-Android-Malware-Defenses-review",
+                    url: "https://github.com/yueyueL/DL-based-Android-Malware-Defenses-review",
+                    type: "repository",
+                    similarity_score: 70,
+                    malicious_level: 2,
+                    features: {
+                        layout_feature: 5
+                    }
+                },
+                {
+                    name: "E0HYL/BrutePrint",
+                    url: "https://github.com/E0HYL/BrutePrint",
+                    type: "repository",
+                    similarity_score: 60,
+                    malicious_level: 9,
+                    features: {
+                        layout_feature: 1
+                    }
+                },
+                {
+                    name: "vxunderground/MalwareSourceCode",
+                    url: "https://github.com/vxunderground/MalwareSourceCode",
+                    type: "repository",
+                    similarity_score: 50,
+                    malicious_level: 2,
+                    features: {
+                        layout_feature: 4
+                    }
+                },
+                {
+                    name: "binary-husky/gpt_academic",
+                    url: "https://github.com/binary-husky/gpt_academic",
+                    type: "repository",
+                    similarity_score: 40,
+                    malicious_level: 9,
+                    features: {
+                        layout_feature: 2
+                    }
+                }
+            ]
+        }
+    ]
+
+    getSimilarRepository(task_id) {
+        for (let i in this.trace_result_list) {
+            if (this.trace_result_list[i].id !== task_id) {
+                continue
+            }
+            return this.trace_result_list[i].similar_repository
+        }
+        return []
+    }
+
     constructor() {
         makeAutoObservable(this)
     }
