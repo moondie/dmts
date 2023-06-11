d82b069a5e491ec1ceee90ba1048ecc835132ebc
liwenke1 <949484025@qq.com>
2023-06-12 03:21:22 +0800

    feat: use g6 to draw intelligence graph

--- src/pages/Layout/Result/ResultIntelligence/index.js
+++ src/pages/Layout/Result/ResultIntelligence/index.js
@@ -4,21 +4,182 @@
  * 以力导向图的形式展示代码归属结果和情报分析结果
  */
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ResultHeader from "../ResultHeader";
 import { useStore } from "@/store";
 import { useSearchParams } from "react-router-dom";
 import G6 from "@antv/g6";
+import { Divider } from "antd";
+
+import { NodeTooltips, EdgeToolTips, NodeContextMenu } from './component'
+
+
+const ResultIntelligenceGraph = ({ task_id }) => {
+    const ref = useRef(null)
+    let graph = null
+    const { resultStore } = useStore()
+    // 边tooltip坐标
+    const [showEdgeTooltip, setShowEdgeTooltip] = useState(false)
+    const [edgeTooltipX, setEdgeTooltipX] = useState(0)
+    const [edgeTooltipY, setEdgeTooltipY] = useState(0)
+
+    // 节点tooltip坐标
+    const [showNodeTooltip, setShowNodeTooltip] = useState(false)
+    const [nodeTooltipX, setNodeToolTipX] = useState(0)
+    const [nodeTooltipY, setNodeToolTipY] = useState(0)
+
+    // 节点ContextMenu坐标
+    const [showNodeContextMenu, setShowNodeContextMenu] = useState(false)
+    const [nodeContextMenuX, setNodeContextMenuX] = useState(0)
+    const [nodeContextMenuY, setNodeContextMenuY] = useState(0)
+
+    const bindEvents = () => {
+        // 监听edge上面mouse事件
+        graph.on('edge:click', evt => {
+            const { item, target } = evt
+            const type = target.get('type')
+            if (type !== 'text') {
+                return
+            }
+            const model = item.getModel()
+            const { endPoint } = model
+            const y = endPoint.y - 35
+            const x = endPoint.x - 150 - 10
+            const point = graph.getCanvasByPoint(x, y)
+            setEdgeTooltipX(point.x)
+            setEdgeTooltipY(point.y)
+            setShowEdgeTooltip(true)
+        })
+
+        graph.on('edge:mouseleave', () => {
+            setShowEdgeTooltip(false)
+        })
+
+        // 监听node上面mouse事件
+        graph.on('node:mouseenter', evt => {
+            const { item } = evt
+            const model = item.getModel()
+            const { x, y } = model
+            const point = graph.getCanvasByPoint(x, y)
+            console.log(evt)
+            setNodeToolTipX(point.x - 75)
+            setNodeToolTipY(point.y + 15)
+            setShowNodeTooltip(true)
+        })
+
+        // 节点上面触发mouseleave事件后隐藏tooltip和ContextMenu
+        graph.on('node:mouseleave', () => {
+            setShowNodeTooltip(false)
+            setShowNodeContextMenu(false)
+        })
+
+        // 监听节点上面右键菜单事件
+        graph.on('node:contextmenu', evt => {
+            const { item } = evt
+            const model = item.getModel()
+            const { x, y } = model
+            const point = graph.getCanvasByPoint(x, y)
+            setNodeContextMenuX(point.x)
+            setNodeContextMenuY(point.y)
+            setShowNodeContextMenu(true)
+        })
+    }
+
+    useEffect(() => {
+        if (!graph) {
+            const miniMap = new G6.Minimap()
+            graph = new G6.Graph({
+                container: ref.current,
+                width: 1000,
+                height: 500,
+                modes: {
+                    default: ['drag-canvas', 'drag-node', 'zoom-canvas']
+                },
+                defaultNode: {
+                    shape: 'node',
+                    size: 15,
+                    // 节点文本样式
+                    labelCfg: {
+                        style: {
+                            fill: '#000000A6',
+                            fontSize: 5
+                        }
+                    },
+                    // 节点默认样式
+                    style: {
+                        stroke: '#72CC4A',
+                        width: 50
+                    }
+                },
+                defaultEdge: {
+                    shape: 'polyline'
+                },
+                // 节点交互状态配置
+                nodeStateStyles: {
+                    hover: {
+                        stroke: 'red',
+                        lineWidth: 3
+                    }
+                },
+                edgeStateStyles: {
+                    hover: {
+                        stroke: 'blue',
+                        lineWidth: 3
+                    }
+                },
+                layout: {
+                    type: 'force',
+                    preventOverlap: true,
+                },
+                plugins: [miniMap]
+            })
+        }
+        graph.data(resultStore.getIntelligenceResult(task_id))
+        graph.render()
+
+        const edges = graph.getEdges()
+        edges.forEach(edge => {
+            const line = edge.getKeyShape()
+            const stroke = line.attr('stroke')
+            const targetNode = edge.getTarget()
+            targetNode.update({
+                style: { stroke }
+            })
+        })
+        graph.paint()
+
+        bindEvents()
+    }, [task_id])
+
+    return (
+        <div ref={ref}>
+            {showEdgeTooltip && <EdgeToolTips x={edgeTooltipX} y={edgeTooltipY} />}
+            {showNodeTooltip && <NodeTooltips x={nodeTooltipX} y={nodeTooltipY} />}
+            {showNodeContextMenu && <NodeContextMenu x={nodeContextMenuX} y={nodeContextMenuY} />}
+        </div>
+    );
+}
 
 const ResultIntelligence = () => {
-    const { resultStore } = useStore()
+
     const [params] = useSearchParams()
     let task_id = parseInt(params.get("task"))
     if (isNaN(task_id)) task_id = "请选择扫描任务";
-    console.log(G6.Global.version)
     return (
         <>
             <ResultHeader></ResultHeader>
+            <div style={{
+                margin: 8,
+                padding: 8,
+                backgroundColor: '#fff',
+                borderRadius: 16,
+                overflow: "auto",
+            }}
+                id="container"
+            >
+                <Divider>代码情报分析结果图</Divider>
+                <ResultIntelligenceGraph task_id={task_id}></ResultIntelligenceGraph>
+            </div>
         </>
     )
 }
--- src/store/result.Store.js
+++ src/store/result.Store.js
@@ -15,6 +15,7 @@ class ResultStore {
         {
             id: 1,
             name: "测试 1",
+            repos_name: "github.com/THUDM/VisualGLM-6B",
             url: "https://github.com/THUDM/VisualGLM-6B",
             type: "代码溯源分析",
             language: 'Python',
@@ -24,7 +25,7 @@ class ResultStore {
             },
             similar_repository: [
                 {
-                    name: "ml-research/XIConceptLearning",
+                    name: "github.com/ml-research/XIConceptLearning",
                     url: "https://github.com/ml-research/XIConceptLearning",
                     type: "repository",
                     similarity_score: 90,
@@ -34,7 +35,7 @@ class ResultStore {
                     }
                 },
                 {
-                    name: "MBCProject/mbc-markdown",
+                    name: "github.com/MBCProject/mbc-markdown",
                     url: "https://github.com/MBCProject/mbc-markdown",
                     type: "repository",
                     similarity_score: 90,
@@ -44,7 +45,7 @@ class ResultStore {
                     }
                 },
                 {
-                    name: "Vision-CAIR/MiniGPT-4",
+                    name: "github.com/Vision-CAIR/MiniGPT-4",
                     url: "https://github.com/Vision-CAIR/MiniGPT-4",
                     type: "repository",
                     similarity_score: 80,
@@ -54,7 +55,7 @@ class ResultStore {
                     }
                 },
                 {
-                    name: "yueyueL/DL-based-Android-Malware-Defenses-review",
+                    name: "github.com/yueyueL/DL-based-Android-Malware-Defenses-review",
                     url: "https://github.com/yueyueL/DL-based-Android-Malware-Defenses-review",
                     type: "repository",
                     similarity_score: 70,
@@ -64,7 +65,7 @@ class ResultStore {
                     }
                 },
                 {
-                    name: "E0HYL/BrutePrint",
+                    name: "github.com/E0HYL/BrutePrint",
                     url: "https://github.com/E0HYL/BrutePrint",
                     type: "repository",
                     similarity_score: 60,
@@ -74,7 +75,7 @@ class ResultStore {
                     }
                 },
                 {
-                    name: "vxunderground/MalwareSourceCode",
+                    name: "github.com/vxunderground/MalwareSourceCode",
                     url: "https://github.com/vxunderground/MalwareSourceCode",
                     type: "repository",
                     similarity_score: 50,
@@ -84,11 +85,51 @@ class ResultStore {
                     }
                 },
                 {
-                    name: "binary-husky/gpt_academic",
+                    name: "github.com/binary-husky/gpt_academic",
                     url: "https://github.com/binary-husky/gpt_academic",
                     type: "repository",
                     similarity_score: 40,
                     malicious_level: 9,
+                    features: {
+                        layout_feature: 2
+                    }
+                },
+                {
+                    name: "github.com/NotDSF/ServerSearch",
+                    url: "https://github.com/NotDSF/ServerSearch",
+                    type: "repository",
+                    similarity_score: 65,
+                    malicious_level: 1,
+                    features: {
+                        layout_feature: 2
+                    }
+                },
+                {
+                    name: "github.com/tedaditya/healthcare-kiosk",
+                    url: "https://github.com/tedaditya/healthcare-kiosk",
+                    type: "repository",
+                    similarity_score: 36,
+                    malicious_level: 0,
+                    features: {
+                        layout_feature: 2
+                    }
+                },
+                {
+                    name: "github.com/untodeath/main",
+                    url: "https://github.com/untodeath/main",
+                    type: "repository",
+                    similarity_score: 45,
+                    malicious_level: 9,
+                    features: {
+                        layout_feature: 2
+                    }
+                },
+                {
+                    name: "github.com/chfrank-cgn/Rancher",
+                    url: "https://github.com/chfrank-cgn/Rancher",
+                    type: "repository",
+                    similarity_score: 75,
+                    malicious_level: 4,
                     features: {
                         layout_feature: 2
                     }
@@ -97,6 +138,61 @@ class ResultStore {
         }
     ]
 
+    /**
+     * @description 情报分析数据
+     */
+    intelligence_result_list = {
+        "github.com/NotDSF/ServerSearch": {
+            url: "https://github.com/NotDSF/ServerSearch",
+            author: "dsf",
+            social_attributes: [
+                {
+                    key: "独立合作",
+                    value: "true",
+                    url: "https://github.com/NotDSF"
+                }
+            ]
+        },
+        "github.com/tedaditya/healthcare-kiosk": {
+            url: "https://github.com/tedaditya/healthcare-kiosk",
+            author: "Alvian Tedy Aditya",
+            social_attributes: [
+                {
+                    key: "消费能力",
+                    value: "中等",
+                    url: "https://twitter.com/tedaditya"
+                }
+            ]
+        },
+        "github.com/untodeath/main": {
+            url: "https://github.com/untodeath/main",
+            author: "Unto Death",
+            social_attributes: [
+                {
+                    key: "宗教背景",
+                    value: "基督教",
+                    url: "https://github.com/untodeath"
+                }
+            ]
+        },
+        "github.com/chfrank-cgn/Rancher": {
+            url: "https://github.com/chfrank-cgn/Rancher",
+            author: "Christian Frank",
+            social_attributes: [
+                {
+                    key: "宗教背景",
+                    value: "佛教",
+                    url: "https://github.com/chfrank-cgn"
+                }
+            ]
+        }
+    }
+
+    /**
+     * @description 根据任务 id 来获取代码归属结果中的相似性的仓库
+     * @param {Number} task_id 
+     * @returns {Array}
+     */
     getSimilarRepository(task_id) {
         for (let i in this.trace_result_list) {
             if (this.trace_result_list[i].id !== task_id) {
@@ -105,6 +201,41 @@ class ResultStore {
             return this.trace_result_list[i].similar_repository
         }
         return []
+    }
+
+    /**
+     * @description 根据任务 id 来获取情报分析结果, 目前结果写死
+     * @param {Number} task_id 
+     * @returns {Object}
+     */
+    getIntelligenceResult(task_id) {
+        let edges = []
+        let nodes = []
+        for (let i in this.trace_result_list) {
+            if (this.trace_result_list[i].id === task_id) {
+                let src_name = this.trace_result_list[i].repos_name
+                nodes.push({
+                    "id": src_name,
+                    "label": src_name
+                })
+                for (let j in this.trace_result_list[i].similar_repository) {
+                    edges.push({
+                        "source": src_name,
+                        "target": this.trace_result_list[i].similar_repository[j].name
+                    })
+                }
+            }
+        }
+        for (let i in edges) {
+            nodes.push({
+                "id": edges[i].target,
+                "label": edges[i].target
+            })
+        }
+        return {
+            "nodes": nodes,
+            "edges": edges
+        }
     }
 
     constructor() {
