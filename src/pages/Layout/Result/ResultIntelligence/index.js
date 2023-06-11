3e43c6d910b7ae3e2b0257bce4ff6588cff47f6b
liwenke1 <949484025@qq.com>
2023-06-11 23:16:50 +0800

    feat: add intelligence result header page

--- src/pages/Layout/Result/ResultIntelligence/index.js
+++ src/pages/Layout/Result/ResultIntelligence/index.js
@@ -5,9 +5,22 @@
  */
 
 import React from "react";
+import ResultHeader from "../ResultHeader";
+import { useStore } from "@/store";
+import { useSearchParams } from "react-router-dom";
+import G6 from "@antv/g6";
 
-const ResultIntelligence = () => (
-    <h2>基于情报分析的社会属性和心理属性分析结果</h2>
-);
+const ResultIntelligence = () => {
+    const { resultStore } = useStore()
+    const [params] = useSearchParams()
+    let task_id = parseInt(params.get("task"))
+    if (isNaN(task_id)) task_id = "请选择扫描任务";
+    console.log(G6.Global.version)
+    return (
+        <>
+            <ResultHeader></ResultHeader>
+        </>
+    )
+}
 
 export default ResultIntelligence
