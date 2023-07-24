b37c2a2a0c69ab95eaaa71a43d349cf58015b1fc
liwenke1 <949484025@qq.com>
2023-07-24 18:32:21 +0800

    feat: update psychological/trace page
    update data format

--- src/pages/Layout/Result/ResultIntelligence/psychological/trace/index.js
+++ src/pages/Layout/Result/ResultIntelligence/psychological/trace/index.js
@@ -1,127 +1,96 @@
 import React from "react";
+import { ProCard } from "@ant-design/pro-components"
 
+import AttributeGraph from "@/pages/Layout/Result/ResultIntelligence/util/AttributeGraph";
 
 const PsychologicalTracePage = () => {
-    const data={
-        nodes:[
+    const data = {
+        nodes: [
             {
-                id:"社会属性空间-职业",
-                label:"社会属性空间-职业",
-                socialAttributes:[
+                id: "aaronesau的社会属性空间",
+                label: "aaronesau的社会属性空间",
+                socialAttributes: [
                     {
                         key: "职业",
                         value: "智能合约审计员和web/pwn技术顾问",
-                        url: "https://aaronesau.com/"
+                        url: ["https://aaronesau.com/"],
                     },
-                   
                 ]
             },
             {
-                id:"互联网工作空间-github",
-                label:"互联网工作空间-github",
-                socialAttributes:[
+                id: "aaronesau的互联网工作空间",
+                label: "aaronesau的互联网工作空间",
+                socialAttributes: [
                     {
-                        key: "github主页",
-                        url: "https://aaronesau.com/"
+                        key: "github用户名",
+                        value: "arinerron",
+                        url: ["https://github.com/arinerron"],
                     },
                     {
                         key: "仓库数量",
-                        value:105
+                        value: 105,
+                        url: ["https://github.com/Arinerron?tab=repositories"],
                     },
                     {
-                        key: "Stars",
-                        value:369
+                        key: "标星数量",
+                        value: 369,
+                        url: ["https://github.com/arinerron"],
                     }
-                   
                 ]
             },
             {
-                id:"互联网社交空间-社交言论分析-领域",
-                label:"互联网社交空间-社交言论分析-领域",
-                socialAttributes:[
+                id: "aaronesau的互联网社交空间",
+                label: "aaronesau的互联网社交空间",
+                socialAttributes: [
                     {
-                        key: "互联网社交空间-社交言论分析-领域",
+                        key: "社交言论分析-领域",
                         value: "计算机",
-                        url: "https://twitter.com/arinerron/status/1448107828846026754"
+                        url: ["https://twitter.com/arinerron/status/1448107828846026754"],
                     }
                 ]
             },
-            
             {
-                id:"性格特质空间/jaywcjlove",
-                label:"性格特质空间/jaywcjlove",
-                socialAttributes:[
+                id: "jaywcjlove的性格特质空间",
+                label: "jaywcjlove的性格特质空间",
+                socialAttributes: [
                     {
-                        id: "github.com/jaywcjlove",
-                        label: "github.com/jaywcjlove",
-                        socialAttributes: [
-                            {
-                                key: "总结",
-                                value: "宜人性和外倾性较高"
-                            }
-                        ],
-                        // 为分析节点单独设置颜色,覆盖 default 配置
-                        style: {
-                            fill: "#32E0C4"
-                        },
-                        labelCfg: {
-                            style: {
-                                fill: "#32E0C4"
-                            }
-                        }
-                    },]
+                        key: "外倾性",
+                        value: "高，个人信息残留较多",
+                        url: ["https://github.com/jaywcjlove"],
+                    },
+                ]
             },
             {
-                id:"互联网学习空间",
-                label:"互联网学习空间",
-                socialAttributes:[
+                id: "hartescout的性格特质空间",
+                label: "hartescout的性格特质空间",
+                socialAttributes: [
                     {
-                        key: "互联网学习空间-提交数",
-                        value: "407提交(今年来)",
+                        key: "神经质",
+                        value: "神经质(情绪稳定性)较低",
+                        url: ["https://github.com/hartescout/exploitdb/blob/016ad02a70d942a7596e7b03164dc986fc38ac09/exploits/linux/remote/437.c#L106"],
+                    }
+                ]
+            },
+            {
+                id: "jaywcjlove的互联网学习空间",
+                label: "jaywcjlove的互联网学习空间",
+                socialAttributes: [
+                    {
+                        key: "代码提交数量",
+                        value: "6.1K",
+                        url: ["https://github.com/jaywcjlove"]
                     },
                     {
-                        key:"备忘清单",
-                        value:"快速参考备忘清单（速查表）"
+                        key: "个人主页类型",
+                        value: "学习笔记和项目介绍",
+                        url: ["https://wangchujiang.com/"],
                     },
-                    {
-                        key: "来源",
-                        value: "github",
-                        url: "https://github.com/jaywcjlove"
-                    }
-                ]
-            },
-            
-            
-            
-            {
-                id:"性格特质空间/hartescout",
-                label:"性格特质空间/hartescout",
-                socialAttributes:[
-                    {
-                        id: "github.com/hartescout",
-                        label: "github.com/hartescout",
-                        socialAttributes: [
-                            {
-                                key: "总结",
-                                value: "开放性较高，神经质(情绪稳定性)较低"
-                            }
-                        ],
-                        // 为分析节点单独设置颜色,覆盖 default 配置
-                        style: {
-                            fill: "#32E0C4"
-                        },
-                        labelCfg: {
-                            style: {
-                                fill: "#32E0C4"
-                            }
-                        }
-                    }
                 ]
             },
             {
-                id:"互联网工作空间",
-                label:"互联网工作空间",
-                socialAttributes:[
+                id: "hartescout的互联网工作空间",
+                label: "hartescout的互联网工作空间",
+                socialAttributes: [
                     {
                         key: "互联网工作空间-提交数",
                         value: "154提交(今年来)",
@@ -138,68 +107,71 @@ const PsychologicalTracePage = () => {
                     }
                 ]
             },
-            
             {
-                id:"互联网娱乐空间",
-                label:"互联网娱乐空间",
-                socialAttributes:[
+                id: "acedrew的互联网娱乐空间",
+                label: "acedrew的互联网娱乐空间",
+                socialAttributes: [
                     {
-                        key: "爱好",
+                        key: "兴趣爱好",
                         value: "摄影",
-                    }   
-                    ]
-                },
-            
-        
-                {
-                    id:"互联网社交空间",
-                    label:"互联网社交空间",
-                    socialAttributes:[
-                        {
-                            key: "来源-互联网社交空间",
-                            value: "mastodon社交平台",
-                            url: "https://mastodon.social/@acedrew"
-                        }   
-                        ]
+                        url: ["https://mastodon.social/@hannah@social.alt-text.org/110759001920740773"],
                     }
-            ]
-            }  
-    edges:[
-        {
-            source: "社会属性空间-职业",
-            target: "互联网社交空间-社交言论分析-领域",
-        },
-        {
-            source: "社会属性空间-职业",
-            target: "互联网工作空间-github",
-        },
-        {
-            source: "性格特质空间/jaywcjlove",
-            target: "互联网学习空间",
-        },
-        {
-            source: "性格特质空间/hartescout",
-            target: "互联网工作空间",
-        },
-        {
-            source: "互联网娱乐空间",
-            target: "互联网社交空间",
-        }
-    ] 
+                ]
+            },
+            {
+                id: "acedrew的互联网社交空间",
+                label: "acedrew的互联网社交空间",
+                socialAttributes: [
+                    {
+                        key: "社交平台",
+                        value: "mastodon",
+                        url: "https://mastodon.social/@acedrew"
+                    }
+                ]
+            }
+        ],
+        edges: [
+            {
+                source: "aaronesau的社会属性空间",
+                target: "aaronesau的互联网社交空间",
+                label: "职业",
+            },
+            {
+                source: "aaronesau的社会属性空间",
+                target: "aaronesau的互联网工作空间",
+                label: "职业",
+            },
+            {
+                source: "jaywcjlove的性格特质空间",
+                target: "jaywcjlove的互联网学习空间",
+                label: "外倾性",
+            },
+            {
+                source: "hartescout的性格特质空间",
+                target: "hartescout的互联网工作空间",
+                label: "神经质",
+            },
+            {
+                source: "acedrew的互联网娱乐空间",
+                target: "acedrew的互联网社交空间",
+                label: "兴趣爱好"
+            }
+        ]
+    }
     return (
         <ProCard
-        split="horizontal"
-        headerBordered
-        bordered
-    >
-        <ProCard
-            split='vertical'
+            split="horizontal"
+            headerBordered
+            bordered
         >
-            <ProCard title="trace">
-                <AttributeGraph data={data}></AttributeGraph>
+            <ProCard
+                split='vertical'
+            >
+                <ProCard title="社交跟踪范围">
+                    <AttributeGraph data={data}></AttributeGraph>
+                </ProCard>
             </ProCard>
-            </ProCard>
-    </ProCard>
+        </ProCard >
     )
 }
 
